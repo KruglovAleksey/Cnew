e7e7fa9ba940ce5f7ad35cc597d241daf80a1113
aleksey <razdolbam@yandex.ru>
2023-06-09 12:23:53 +0300

    add new5

---
+++ new5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя ");
+string usermane = Console.ReadLine();
+
+if(usermane.ToLower() == "маша")
+{
+    Console.WriteLine("Ура это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(usermane);
+}
